af27d1f5e0d6f69b4e91ddcaee650d59b932d807
qauseqazah <qaus_e_qazah@yahoo.com>
2015-07-25 03:20:05 +0500

    commit

--- ClusterCart/Global.asax.cs
+++ ClusterCart/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ClusterCart.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Optimization;
@@ -16,6 +18,9 @@ namespace ClusterCart
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialize the product database. 
+            Database.SetInitializer(new ProductDatabaseInitializer());
         }
     }
 }
